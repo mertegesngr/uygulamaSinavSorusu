f136ea81658c4c672c0f2ce742011ad8de5a25b0
mertegesngr <mertegesungur5@gmail.com>
2024-11-05 13:49:58 +0300

    Update sinav.c

--- sinav.c
+++ sinav.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
-int main(){
 
-int x ; 
+int main() {
+    int x; 
+    int basamak = 0; 
+    int sayac = 0;
 
-int basamak =0 ; 
-int sayac=0 ; 
+    // Kullanıcıdan sürekli olarak rakam girişi istenen döngü
+    while (1) {
+        sayac++;
+        printf("Sayı giriniz (Çıkmak için -1 giriniz): ");
+        scanf("%d", &x);
 
-
-while (1){
-sayac++ ; 
-printf("sayı giriniz : "); scanf("%d",&x) ; 
-
-if(x<=9 && x>=0){
-basamak = (basamak*10) +x ; 
-
-}
-
-else if(x>9 || x<-1) {
-
-    printf("hatalı bir sayı girdiniz 0-9 arası rakam girilmelidir") ; printf("\n") ;
-}
-
-
-
-
-    if(x==-1){
-        
-
-printf("programdan çıkış yapıldı\n") ; 
-        break; 
-        
+        // Girilen sayı 0-9 aralığındaysa `basamak` değerine eklenir
+        if (x >= 0 && x <= 9) {
+            basamak = (basamak * 10) + x;  // Yeni basamak oluşturuluyor
+        }
+        // Girilen sayı -1 ise programdan çıkış yapılır
+        else if (x == -1) {
+            printf("Programdan çıkış yapıldı.\n");
+            break;  // Döngü sonlandırılır
+        }
+        // 0-9 aralığında değil ve -1 değilse hata mesajı verilir
+        else {
+            printf("Hatalı bir sayı girdiniz. 0-9 arası rakam girilmelidir.\n");
+        }
     }
-   
-}
 
+    // Döngü bittikten sonra elde edilen sonucu ekrana yazdır
+    printf("Sonuç = %d\n", basamak);
 
-
-if (sayac<=1){
-
-printf("sonuç =%d",basamak) ;  
-
-}
-
-
-
-
-
-
-
-    return 0 ; 
+    return 0;
 }
